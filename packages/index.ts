56f773948f0ca20f1f679532d9fdabf2e7eefa3b
wujihua <wjh50940@163.com>
2022-12-26 10:52:04 +0800

    feat: add useControlled hooks

--- packages/index.ts
+++ packages/index.ts
@@ -1,5 +1,31 @@
-const useControlled = () => {
-  console.log('useControlled test')
+import * as React from 'react'
+import upperFirst from 'lodash.upperfirst'
+
+export interface ChangeHandler<T, A extends any[]> {
+  (value: T, ...args: A): void
+}
+
+const noop = () => {}
+
+const useControlled = <T, A extends any[]>(
+  props: Record<string, any> = {},
+  valueKey: string,
+  onChange: ChangeHandler<T, A>
+): [T, ChangeHandler<T, A>] => {
+  const controlled = Reflect.has(props, valueKey)
+  const value = props[valueKey]
+  const defaultValue = props[`default${upperFirst(valueKey)}`]
+  const [internalValue, setInternalValue] = React.useState(defaultValue)
+
+  if (controlled) return [value, onChange || noop]
+
+  return [
+    internalValue,
+    (newValue, ...args) => {
+      setInternalValue(newValue)
+      onChange?.(newValue, ...args)
+    },
+  ]
 }
 
 export default useControlled
